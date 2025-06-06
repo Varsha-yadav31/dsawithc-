3a5506a9bd919b3d2c05e711dcf8538147134f86
Varsha-yadav31 <varshu31yadav@gmail.com>
2025-06-06 13:09:52 +0530

    add new code

---
+++ conditionalstatment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+    
+    if (age >= 18) {
+        cout << "Can vote" << endl;
+    } else {
+        cout << "Can't vote" << endl;
+    }
+    return 0;
+}
+
+//find upper and lower case latter
+
+
+
