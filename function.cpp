1fc5acb9800e9e2567aebb0ce34052ffccbdcb9a
Varsha-yadav31 <varshu31yadav@gmail.com>
2025-06-22 18:11:19 +0530

    Update: add sum and minOfTwo functions with printHello

--- function.cpp
+++ function.cpp
@@ -1,14 +1,27 @@
-  //function is a black box that tack the input and return the output
-  
-  #include<iostream>
-  using namespace std;
-  //function difine kr rhe hain 
-   void printHello(){
+//function is a black box that tack the input and return the output
+
+#include<iostream>
+using namespace std;
+//function difine kr rhe hain 
+ void printHello(){
     cout<<"hlo\n";
+   }
+   double sum(int a ,int b){
+    double s = a + b;
+    return s;
+
+   }
+   int minOfTwo(int a,int b){
+    if(a<b){
+        return a;
+    }else{
+        return b;
+    }
    }
    int main(){
     //func ko call krte hain /invock the function
     printHello();
+    cout<<"min ="<<minOfTwo(5,3)<<endl;
     return 0;
 
    }
