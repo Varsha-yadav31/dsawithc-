6f16f6ae4f8a7134116ef19377559e56f8f8cb24
Varsha-yadav31 <varshu31yadav@gmail.com>
2025-06-22 17:58:16 +0530

    Add: simple printHello function example

---
+++ function.cpp
@@ -0,0 +1,14 @@
+  //function is a black box that tack the input and return the output
+  
+  #include<iostream>
+  using namespace std;
+  //function difine kr rhe hain 
+   void printHello(){
+    cout<<"hlo\n";
+   }
+   int main(){
+    //func ko call krte hain /invock the function
+    printHello();
+    return 0;
+
+   }
