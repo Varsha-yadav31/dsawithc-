313fbab12793ef46e9d54ceaf400ba3dfd28be1f
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-01-22 19:57:01 +0530

    day1

--- check_even_odd.cpp
+++ check_even_odd.cpp
@@ -1,1 +1,16 @@
-added new practices in this
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(n % 2 == 0)
+        cout << "Even";
+    else
+        cout << "Odd";
+
+    return 0;
+}
